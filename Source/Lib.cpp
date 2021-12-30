08f2bacd101fd6469dfffc03e7c293d6ddc9699b
Patrick Kappl <patrick.kappl@spaceteam.at>
2021-12-30 16:11:02 +0100

    Disable all CI jobs that require a build
    Building requires RODOS which is not available on the CI servers.
    Therefore only the lint checks can be run with CI right now.

--- Source/Lib.cpp
+++ Source/Lib.cpp
@@ -3,7 +3,6 @@
 using namespace std::literals;
 
 library::library()
-    // : name{'R', 'o', 'd', 'o', 's', 'P', 'l', 'a', 'y', 'g', 'r', 'o', 'u', 'n', 'd', '\0'}
     : name("RodosPlayground"sv)
 {
 }
