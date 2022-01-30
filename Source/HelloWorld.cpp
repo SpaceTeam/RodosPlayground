af3b9860164263ef3c23f751d81477037e817823
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-01-30 22:33:08 +0100

    Revert "Add blinky LED to HelloWorld.cpp"
    This reverts commit 07bf397bc77ec24ecaedc52e39f7d8e881d8a27b.

--- Source/HelloWorld.cpp
+++ Source/HelloWorld.cpp
@@ -5,42 +5,19 @@
 
 namespace rpg
 {
-constexpr auto eduUpdatePin = GPIO_017;  // PB01
-auto blinkyPin = HAL_GPIO(eduUpdatePin);
-
-
 class HelloWorld : public StaticThread<>
 {
-  void init() override
-  {
-    blinkyPin.init(/*isOutput=*/true);
-    blinkyPin.setPins(1);
-  }
-
   void run() override
   {
     auto const lib = Library();
-    auto toggle = true;
-    TIME_LOOP(0, 500 * MILLISECONDS)
+    TIME_LOOP(0, 1 * RODOS::SECONDS)
+    if(lib.name.length() > 0)
     {
-      if(toggle)
-      {
-        blinkyPin.setPins(0);
-      }
-      else
-      {
-        blinkyPin.setPins(1);
-      }
-      toggle = !toggle;
-
-      if(lib.name.length() > 0)
-      {
-        PRINTF("Hello World from %s!\n", lib.name.data());
-      }
-      else
-      {
-        PRINTF("Nothing to print!\n");
-      }
+      PRINTF("Hello World from %s!\n", lib.name.data());
+    }
+    else
+    {
+      PRINTF("Nothing to print!\n");
     }
   }
 };
