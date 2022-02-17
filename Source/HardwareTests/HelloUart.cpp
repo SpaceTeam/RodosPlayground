7f0ea67dcb4f97a0fb8a04703cde222c1e4b79f2
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-02-18 00:20:04 +0100

    Clean up HelloUart.cpp a bit

--- Source/HardwareTests/HelloUart.cpp
+++ Source/HardwareTests/HelloUart.cpp
@@ -5,17 +5,10 @@
 
 namespace rpg
 {
-// UART 1 Board: flying wire from CN10 21 and CN10 33
-// auto uart1 = HAL_UART(UART_IDX1, GPIO_009, GPIO_010);
-// UART6 Board : flying wire from CN10/19 to TX, CN10/04 to Rx
-// auto uart6 = HAL_UART(UART_IDX6, GPIO_038, GPIO_037);
-
-// For COBC (not tested yet)
-// PA10 USART1 RX
-// PA15 USART1 TX
-// TODO: See if this still works if uart1 is made const
+constexpr auto pa10 = GPIO_010;
+constexpr auto pa15 = GPIO_015;
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-auto uart1 = HAL_UART(UART_IDX1, GPIO_015, GPIO_010);
+auto eduUart = HAL_UART(UART_IDX1, pa15, pa10);
 
 
 class HelloUart : public StaticThread<>
@@ -23,7 +16,7 @@ class HelloUart : public StaticThread<>
   void init() override
   {
     constexpr auto baudrate = 115'200;
-    uart1.init(baudrate);
+    eduUart.init(baudrate);
   }
 
   void run() override
@@ -42,7 +35,7 @@ class HelloUart : public StaticThread<>
       else
       {
         auto myText = "Hello from UART1\n"sv;
-        uart1.write(myText.data(), myText.size());
+        eduUart.write(myText.data(), myText.size());
       }
       toggle = not toggle;
     }
