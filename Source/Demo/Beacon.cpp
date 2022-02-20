874af3acac9dd1232c24602895e3e847b6a69387
Jerome Hue <hue.jerome@yahoo.fr>
2022-02-20 19:47:45 +0100

    Fix Beacon.cpp

--- Source/Demo/Beacon.cpp
+++ Source/Demo/Beacon.cpp
@@ -5,11 +5,8 @@
 #include <cstddef>
 #include <cstring>
 
-auto const green = GPIO_005;
 auto const eduHearbeatPin = GPIO_037;  // PC5
 
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-HAL_GPIO greenLed(green);
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 HAL_GPIO eduHeartbeat(eduHearbeatPin);
 
@@ -26,36 +23,37 @@ class HelloWorld : public StaticThread<>
 {
   void init() override
   {
-    greenLed.init(/*isOutput=*/true, 1, 0);
     eduHeartbeat.init(/*isOutput=*/false, 1, 0);
+  }
+
+  static auto ConstructBeacon(int32_t timestamp, int32_t eduHeartbeatvalue)
+  {
+    auto const beaconSize = sizeof(eduHeartbeatvalue) + sizeof(timestamp) + sizeof('?');
+    std::array<std::byte, beaconSize> beacon{};
+
+    size_t i = 0;
+
+    beacon[i] = static_cast<std::byte>('?');
+    i += sizeof('?');
+    std::memcpy(&beacon[i], &timestamp, sizeof(timestamp));
+    i += sizeof(timestamp);
+    std::memcpy(&beacon[i], &eduHeartbeatvalue, sizeof(timestamp));
+    i += sizeof(eduHeartbeatvalue);
+
+    return beacon;
   }
 
   void run() override
   {
     TIME_LOOP(0, 5000 * MILLISECONDS)
     {
-      // Fetch values
       auto eduHeartbeatvalue = static_cast<int32_t>(eduHeartbeat.readPins());
       auto timestamp = static_cast<int32_t>(NOW() / MILLISECONDS);
 
-      auto const beaconSize = sizeof(eduHeartbeatvalue) + sizeof(timestamp) + sizeof('?');
-      std::array<std::byte, beaconSize> beacon{};
-
-      Serialize(beacon, timestamp, eduHeartbeatvalue);
+      auto beacon = ConstructBeacon(timestamp, eduHeartbeatvalue);
 
       uart_stdout.write(beacon.data(), beacon.size());
     }
-  }
-
-  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-  static void Serialize(std::array<std::byte, 9> beacon,
-                        int32_t timestamp,
-                        int32_t eduHeartbeatvalue)
-  {
-    beacon[0] = static_cast<std::byte>('?');
-    std::memcpy(&beacon[1], &timestamp, sizeof(timestamp));
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-    std::memcpy(&beacon[5], &eduHeartbeatvalue, sizeof(timestamp));
   }
 };
 
