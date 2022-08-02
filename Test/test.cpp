749d1812ee2333e87112d20572db1b8f9417d7d7
Jerome Hue <hue.jerome@yahoo.fr>
2022-08-02 11:48:03 +0200

    Refactor testing and change CI accordingly

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,21 +1,34 @@
-#include <catch2/catch_test_macros.hpp>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "UtilityMock.hpp"
 
-unsigned int Factorial(unsigned int number)
+#include <catch2/catch.hpp>
+
+namespace ts = type_safe;
+using ts::operator""_usize;
+
+auto Factorial(unsigned int number) -> unsigned int
 {
     return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST_CASE("Factorials are computed 1", "[factorial]")
 {
-    // Should pass
     REQUIRE(Factorial(1) == 1);
     REQUIRE(Factorial(2) == 2);
     REQUIRE(Factorial(3) == 6);
     REQUIRE(Factorial(10) == 3628800);
 }
 
-TEST_CASE("Factorials are computed fail", "[factorial]")
+TEST_CASE("Factorials are computed 2", "[factorial]")
 {
-    // Should fail
     REQUIRE(Factorial(0) == 1);
+}
+
+TEST_CASE("CopyTo Position Offset", "[Utility]")
+{
+    constexpr auto startByte = '?';
+    auto beacon = std::array<std::byte, 4>{};
+    auto position = 0_usize;
+    rpg::util::CopyTo(beacon, &position, startByte);
+    REQUIRE(position.get() == sizeof(startByte));
 }
